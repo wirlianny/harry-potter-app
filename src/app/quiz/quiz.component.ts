4503ff5e0f151724b8fb0aeaea0529a8c0379f60
Wirlianny <wirlianny@gmail.com>
2022-03-11 00:14:07 -0300

    adicionado logo das casas ao resultado

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -151,23 +151,27 @@ export class QuizComponent implements OnInit {
 
   resultadoCasa(){
 
-
     if(this.points <= 820){
-     return "Grifinória"
+     (<HTMLImageElement>document.querySelector("img")).src="https://senhoritadoestilo.files.wordpress.com/2015/09/5b638-brasao_grifinoria_brhp_by_alugok.jpg"
+     return "Bem-vindo a casa Grifinória!!"
     }
 
     else if (this.points <= 1000){
-      return "Sonserina"
+      (<HTMLImageElement>document.querySelector("img")).src="http://pm1.narvii.com/7350/2f76b6a123c86007ce077676c2d0bdb2aa7bb558r1-736-1308v2_uhq.jpg"
+      return "Bem-vindo a casa Sonserina!!"
     }
     else if(this.points <= 1220) {
-      return "Lufa-lufa"
+      (<HTMLImageElement>document.querySelector("img")).src="https://http2.mlstatic.com/D_NQ_NP_720746-MLB43524083051_092020-O.jpg"
+      return "Bem-vindo a casa Lufa-lufa!!"
     }
     else if (this.points <= 1495){
-      return "Corvinal"
+      (<HTMLImageElement>document.querySelector("img")).src="https://pbs.twimg.com/profile_images/1038240488954970112/YbPjAxYI_400x400.jpg"
+      return "Bem-vindo a casa Corvinal!!"
     } else {
       return null
     }
   }
+
 
 
 
